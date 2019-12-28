d1cb173e3f280f7eb067f2ac21a3e1bde5aeff2b
Sebastian Seggewiss <sebastianseggewiss@gmail.com>
2019-12-28 20:21:18 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace GcodeRenumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 2) {
+                Console.WriteLine("Please provide 3 arguments: NumerationStart, Step and Input file.");
+                return;
+            }
+
+            int start, step;
+            string path;
+
+            start = Convert.ToInt32(args[0]);
+            step = Convert.ToInt32(args[1]);
+            path = args[2];
+
+            string[] lines = System.IO.File.ReadAllLines(path);
+            lines = Renumber(start, step, lines);
+            
+            System.IO.File.WriteAllLines(path, lines);
+        }
+
+        private static string[] Renumber(int start, int step, string[] lines)
+        {
+            int lineNumber = start;
+            int count = 0;
+            string[] newLines = new string[lines.Length];
+
+            foreach (var line in lines) {
+                newLines[count] = RenumberLine(line, lineNumber);
+                count++;
+                lineNumber += step;
+            }
+
+            return newLines;
+        }
+
+        private static string RenumberLine(string line, int lineNumber)
+        {
+            var match = Regex.Match(line, "[nN][\\d]*");
+
+            if (match.Success) {
+                line = line.Replace(match.Value, "N" + lineNumber);
+
+                return line;
+            }
+
+            return "N" + lineNumber + " " + line;
+        }
+    }
+}
